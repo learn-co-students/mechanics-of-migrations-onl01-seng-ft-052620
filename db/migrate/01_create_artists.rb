863b92b99171d187243830294e0cee8d9acc2bc3
Donna Torres <todonnatorres@icloud.com>
2020-07-01 22:22:39 -0500

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,10 +1,10 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
 
-    def up
-    end
+    # def up
+    # end
    
-    def down
-    end
+    # def down
+    # end
 
     def change
         create_table :artists do |t|
